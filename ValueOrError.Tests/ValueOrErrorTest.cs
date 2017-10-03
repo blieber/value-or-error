9e3c86525e1a535a4bd0850b00997dd0a6db7f49
Ben Lieber <ben@quantac.co>
2017-10-03 10:20:50 -0400

    Flush out ValueOrError tests

--- ValueOrError.Tests/ValueOrErrorTest.cs
+++ ValueOrError.Tests/ValueOrErrorTest.cs
@@ -1,14 +1,57 @@
 using System;
 using Xunit;
+using ErrorFlow;
 
-namespace ValueOrError.Tests
+namespace ErrorFlow.Tests
 {
-    public class ValueOrErrorTest
+    public class ValueOrErrorTests
     {
-        [Fact]
-        public void Test1()
+        public class FromValue 
         {
-            Assert.False(true);
+            [Fact]
+            public void Value()
+            {
+                var valueOrError = ValueOrError<bool, string>.FromValue(true);
+                Assert.True(valueOrError.value);
+            }
+
+            [Fact]
+            public void Error()
+            {
+                var valueOrError = ValueOrError<bool, string>.FromValue(true);
+                Assert.Throws<InvalidOperationException>(() => valueOrError.error);
+            }
+
+            [Fact]
+            public void HasValue()
+            {
+                var valueOrError = ValueOrError<bool, string>.FromValue(true);
+                Assert.True(valueOrError.hasValue);
+            }
+        }
+
+        public class FromError 
+        {
+            [Fact]
+            public void Value()
+            {
+                var valueOrError = ValueOrError<bool, string>.FromError("I'm an error");
+                Assert.Throws<InvalidOperationException>(() => valueOrError.value);
+            }
+
+            [Fact]
+            public void Error()
+            {
+                var valueOrError = ValueOrError<bool, string>.FromError("I'm an error");
+                Assert.Equal("I'm an error", valueOrError.error);
+            }
+
+            [Fact]
+            public void HasValue()
+            {
+                var valueOrError = ValueOrError<bool, string>.FromError("I'm an error");
+                Assert.False(valueOrError.hasValue);
+            }
         }
     }
 }
